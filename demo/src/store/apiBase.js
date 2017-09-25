a11b2a3df503ee0bc91ee83430000d00ea672186
geohuz@163.com <geohuz@163.com>
2017-09-25 19:43:00 +0800

    Minor syntax fixing

--- demo/src/store/apiBase.js
+++ demo/src/store/apiBase.js
@@ -3,7 +3,7 @@ import errorLog from './errorLog'
 import {process} from 'mobx-state-tree'
 
 export const dataApi = axios.create({
-  baseURL: 'http://123.57.154.181:3000',
+  baseURL: 'http://123.123.123.123:3000',
   timeout: 10000
 })
 
